213bb040622ee971e1981b44af22d121cf42da99
ireneclay01 <irenekalinga01@gmail.com>
2026-05-28 16:11:38 +0300

    Update main.js

--- frontend/main.js
+++ frontend/main.js
@@ -2,7 +2,7 @@
    main.js
 ══════════════════════════════════════ */
 
-const API_BASE = "http://localhost:5000/api";
+const API_BASE = "portifolio-o3ecqorua-ireneclay01s-projects.vercel.app";
 
 /* ── FETCH HELPER ── */
 
